a25dfb9d5a25614193f1c15f1aebf5197bb29ec5
Sarad Babu Shrestha <saradbabushrestha@Sarads-MacBook-Air.local>
2022-05-05 13:19:32 +0545

    Area Of Triangle

---
+++ 1_introduction/1_aot.cpp
@@ -0,0 +1,16 @@
+// q.WAP to calculate area of triangle
+#include<iostream>
+using namespace std;
+int main (){
+    int base,height,area;
+    cout <<"Enter value of base-";
+    cin>>base;
+    cout <<"Enter value of height";
+    cin>>height;
+    area =0.5*(base * height);
+    cout <<"Area of triangle is"<<area;
+
+    return 0;
+
+
+}
